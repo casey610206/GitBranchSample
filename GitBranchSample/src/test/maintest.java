1962321214b19b2c0ee1226405ab47c0af64083f
happy610206 <happy610206@gmail.com>
2022-02-10 12:31:08 +0800

    備註欄

---
+++ GitBranchSample/src/test/maintest.java
@@ -0,0 +1,13 @@
+package test;
+
+public class maintest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//這是組長的第一次編輯
+		System.out.println("1st version");
+
+	}
+
+}
